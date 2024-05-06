ccfcdf4c798519c5abd817a8dee7f8e1842cd862
u2024 <157313309+u2024@users.noreply.github.com>
2024-05-06 20:13:15 +0200

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -7,20 +7,13 @@ std::deque<std::string> lengthSort(std::forward_list<std::string> &list)
 {
     std::deque<std::string> deque;
     list.sort();
-    for(auto el : list)
-    {
+    for(auto el : list) {
         deque.push_back(el);
     }
-    for(int i = 0; i < deque.size(); ++i)
-    {
-        auto x = deque[i].size();
-        for(int k = 0; k < deque.size(); ++k)
-        {
-            auto y = deque[k].size();
-            if(x < y)
-            {
-                x = deque[k].size();
-                std::swap(deque[i], deque[k]);
+    for(int i = 1; i < deque.size(); ++i) {
+        for(int k = i; k > 0; --k) {
+            if(deque[k].size() < deque[k-1].size()) {
+                std::swap(deque[k], deque[k-1]);
             }
         }
     }
