5fd9a54f06f14201ddc9fe48c367b3c38bd752e4
u2024 <157313309+u2024@users.noreply.github.com>
2024-05-06 18:19:47 +0200

    Update sort.hpp

--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -1,24 +1,3 @@
 #pragma once
 
-auto lengthSort(std::forward_list<std::string> &list)
-{
-    std::deque<std::string> deque;
-    list.sort();
-    for(auto el : list)
-    {
-        deque.push_back(el);
-    }
-    for(int i = 0; i < deque.size(); ++i)
-    {
-        auto x = deque[i].size();
-        for(int k = 0; k < deque.size(); ++k)
-        {
-            auto y = deque[k].size();
-            if(x < y)
-            {
-                std::swap(deque[i], deque[k]);
-            }
-        }
-    }
-    return deque;    
-}
+auto lengthSort(std::forward_list<std::string> &list);
