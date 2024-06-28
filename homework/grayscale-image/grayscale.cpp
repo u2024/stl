3c849aeba6ec3fde25d23888881a0fdf8f2d24ee
u2024 <157313309+u2024@users.noreply.github.com>
2024-06-28 15:23:40 +0200

    Create grayscale.cpp

---
+++ homework/grayscale-image/grayscale.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <utility>
+#include <array>
+#include <cstdint>
+
+auto compressGrayscale(std::array<std::array<uint8_t, 240>, 160>& array)
+{
+    std::vector<std::pair<uint8_t, uint8_t>> vec;
+    uint8_t count = 0, value1 = array[0][0];
+    for(int k = 0; k < array.size(); ++k)
+    {
+        for(int i = 0; i < array[k].size(); ++i)
+        {
+            if(array[k][i] != value1 && count != 0)
+            {
+                vec.push_back(std::make_pair(value1, count));
+                count = 0;
+                value1 = array[k][i];
+                ++count;
+            }
+            else ++count;
+        }
+        if(count != 0 && k == array.size() - 1)
+        {
+            vec.push_back(std::make_pair(value1, count));
+            count = 0;
+        }
+    }
+    return vec;
+}
+
+auto decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> &vec)
+{
+    std::array<std::array<uint8_t, 240>, 160> array;
+    int l = 0, m = 0;
+    for(int i = 0; i < array.size(); ++i)
+    {
+        for(int k = 0; k < array[i].size(); ++k)
+        {
+            if(vec[l].second != m)
+            {
+                array[i][k] = vec[l].first;
+                ++m;
+            }
+            else 
+            {
+                while(vec[l].second == m || vec[l].second == 0)
+                {
+                    ++l;
+                    m = 0;
+                }
+                array[i][k] = vec[l].first;
+                ++m;
+            }
+        }
+    }
+    return array;
+}
