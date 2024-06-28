00eea9db14133a61b7f14c67f672c0c8fa5ae11c
u2024 <157313309+u2024@users.noreply.github.com>
2024-06-28 15:34:36 +0200

    Update compression.hpp

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -3,8 +3,9 @@
 #include <utility>
 #include <array>
 #include <cstdint>
+#include <cstddef>
 
 constexpr size_t width = 32;
 constexpr size_t height = 32;
-std::array<std::array<uint8_t, 240>, 160> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> &vec);
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 240>, 160>& array);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> &vec);
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>& array);
