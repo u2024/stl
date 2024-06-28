c5e0852c8743709e508aa705f9bc48e7f798a18b
u2024 <157313309+u2024@users.noreply.github.com>
2024-06-28 15:37:55 +0200

    Update compression.cpp

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -3,6 +3,9 @@
 #include <array>
 #include <cstdint>
 
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>& array)
 {
     std::vector<std::pair<uint8_t, uint8_t>> vec;
