26b5062155b303fd6d8f309acc51c754abcdb52c
u2024 <157313309+u2024@users.noreply.github.com>
2024-07-02 12:40:01 +0200

    Create insensitive_palindrom.cpp

---
+++ homework/insensitive-palindrom/insensitive_palindrom.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <string>
+
+bool is_palindrome(std::string &str)
+{
+    bool b = true;
+    for(int i = 0; i < str.size() && b; ++i)
+    {
+        for(int k = str.size() - 1; k > -1 && b; --k)
+        {
+            while(isblank(str[i]) || ispunct(str[i]) || iscntrl(str[i]))
+            {
+                ++i;
+            }
+            while(isblank(str[k]) || ispunct(str[k]) || iscntrl(str[k]))
+            {
+                --k;
+            }
+            if(toupper(str[k]) == toupper(str[i]))
+            {
+                ++i;
+                continue;
+            }
+            else b = false;
+        }
+    }
+    return b;
+}
