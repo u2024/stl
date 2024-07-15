3992780807ce0fbf3f70f2b87d41c920039106f3
u2024 <157313309+u2024@users.noreply.github.com>
2024-07-15 21:38:54 +0200

    Update test.cpp

--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -13,12 +13,20 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     first = {12, 14, 18, 20};
     second = {22, 24, 26, 30};
     expected = 20.75;
-    EXPECT_NEAR(Distance(first, second), expected, 0.00001);
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);
     
     first = {3, 141, 1, 0, 2, 718281828};
     second = {1, 22000, -21432, 34567, -2, 123};
     expected = 59859769.333333;
-    EXPECT_NEAR(Distance(first, second), expected, 0.00001);
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);
+
+    first = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+    second = {1, 3, 9, 27, 81, 243, 729, 2187, 6561};
+    expected = 575,111111111;
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);
+
+    first = {1, -1, 2, -2, 3, -3, 4, -4}
+    second = {
 }
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
