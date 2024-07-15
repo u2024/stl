b9d6469dd4668daa06be14ae658dbb0e3ef7b508
u2024 <157313309+u2024@users.noreply.github.com>
2024-07-15 17:34:40 +0200

    Update test.cpp

--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -9,7 +9,16 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
     std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
     EXPECT_NEAR(ArithmeticAverage(first, second), 2.875, epsilon);
-    // Write more test case
+    
+    first = {12, 14, 18, 20};
+    second = {22, 24, 26, 30};
+    expected = 20.75;
+    EXPECT_NEAR(Distance(first, second), expected, 0.00001);
+    
+    first = {3, 141, 1, 0, 2, 718281828};
+    second = {1, 22000, -21432, 34567, -2, 123};
+    expected = 59859769.333333;
+    EXPECT_NEAR(Distance(first, second), expected, 0.00001);
 }
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
@@ -22,5 +31,15 @@ TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     second = {17, 6, 2, -2};
     expected = 18.16590212458495;
     EXPECT_NEAR(Distance(first, second), expected, 0.00001);
-    // Write more test case
+
+    first = {7, 4, 3, 13};
+    second = {17, 6, 2, -2};
+    expected = 18.16590212458495;
+    EXPECT_NEAR(Distance(first, second), expected, 0.00001);
+
+    first = {7, 4, 3, 13};
+    second = {17, 6, 2, -2};
+    expected = 18.16590212458495;
+    EXPECT_NEAR(Distance(first, second), expected, 0.00001);
+
 }
