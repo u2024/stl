89307b0e3f151109643c7be38aa08bd8326b9102
u2024 <157313309+u2024@users.noreply.github.com>
2024-07-16 14:24:26 +0200

    Update test.cpp

--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -12,7 +12,7 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     
     first = {12, 14, 18, 20};
     second = {22, 24, 26, 30};
-    expected = 20.75;
+    double expected = 20.75;
     EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);
     
     first = {3, 141, 1, 0, 2, 718281828};
@@ -22,17 +22,17 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
 
     first = {1, 2, 4, 8, 16, 32, 64, 128, 256};
     second = {1, 3, 9, 27, 81, 243, 729, 2187, 6561};
-    expected = 575,111111111;
+    expected = 575.111111111;
     EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);
 
     first = {1, -1, 2, -2, 3, -3, 4, -4};
     second = {2, 3, 5, 7, 11, 13, 17, 19};
-    expected = 4,8125;
+    expected = 4.8125;
     EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);
 
     first = {4087, 1740, 5502, 7572, 7235};
     second = {4991, 3441, 5031, 4869, 5083};
-    expected = 4955,1;
+    expected = 4955.1;
     EXPECT_NEAR(ArithmeticAverage(first, second), expected, 0.00001);    
 }
 
@@ -49,21 +49,21 @@ TEST(arithmeticAverageTest, ShouldCalculateDistance) {
 
     first = {1, 2, 3, 4, 5};
     second = {-10, 0, -234567, 684, 11};
-    expected = 234570,985974395;
+    expected = 234570.985974395;
     EXPECT_NEAR(Distance(first, second), expected, 0.00001);
 
     first = {-2, 2};
     second = {0, 1};
-    expected = 2,236067977;
+    expected = 2.236067977;
     EXPECT_NEAR(Distance(first, second), expected, 0.00001);
 
     first = {1, 5, 10};
     second = {0, 0, 0};
-    expected = 11,22497216;
+    expected = 11.22497216;
     EXPECT_NEAR(Distance(first, second), expected, 0.00001);
 
     first = {1, 4, 9, 16, 25, 36, 49};
     second = {3, 1, 4, 1, 5, 9, 2};
-    expected = 60,008332755;
+    expected = 60.008332755;
     EXPECT_NEAR(Distance(first, second), expected, 0.00001);    
 }
